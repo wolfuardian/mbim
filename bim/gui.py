b78d83cf505072c9360937126712f4ba0b1015a8
wolfuardian <wolfuardian@gmail.com>
2023-08-29 23:20:05 +0800

    + add gui script as the main interface gateway

---
+++ bim/gui.py
@@ -0,0 +1,45 @@
+from maya import cmds
+from bim.ui import ui_tweak
+from bim.tools import packages
+
+import bim.tools as tools
+
+global instance
+
+
+def show():
+    global instance
+    tools.Logging.gui_logger().info("Reloading mbim packages")
+    tools.Packages.reload(packages=["bim"])
+
+    try:
+        if instance:
+            tools.Logging.gui_logger().info("Closing GUI")
+            instance.close()
+            instance.deleteLater()
+            instance = ui_tweak.Tweak()
+            instance.update()
+            tools.Logging.gui_logger().info("Showing GUI")
+            instance.show(dockable=True, area='left')
+
+    except NameError:
+        instance = ui_tweak.Tweak()
+        tools.Logging.gui_logger().warning("NameError")
+        tools.Logging.gui_logger().info("Showing GUI")
+        show()
+
+    except RuntimeError:
+        instance = ui_tweak.Tweak()
+        tools.Logging.gui_logger().warning("RuntimeError")
+        tools.Logging.gui_logger().info("Showing GUI")
+        show()
+
+
+def undo():
+    tools.Logging.maya_logger().info("Operating undo")
+    cmds.undo()
+
+
+def redo():
+    tools.Logging.maya_logger().info("Operating redo")
+    cmds.redo()
