0b330e0326933943423a813e52b84171a79b4d27
eos.chen <eos.chen@nadisystem.com>
2023-08-30 19:26:52 +0800

    + update UI layout for better user experience

--- bim/ui/ui_setup.py
+++ bim/ui/ui_setup.py
@@ -7,68 +7,37 @@ import bim.module as module
 class Setup(mayaMixin.MayaQWidgetDockableMixin, mayaMixin.MayaQWidgetBaseMixin, qt.QtDefaultCSWidget):
     def __init__(self, parent=tools.Maya.get_main_window()):
         super(Setup, self).__init__(parent)
-
         self.setWindowTitle("MBIM-2308-0000")
         self.setMinimumWidth(360)
 
+        # <editor-fold desc="CODE_BLOCK: Initialize">
         layout = qt.QtWidgets.QVBoxLayout()
         layout.setAlignment(qt.QtCore.Qt.AlignTop)
         layout.setContentsMargins(3, 3, 3, 3)
         layout.setSpacing(0)
+        # </editor-fold>
 
         # <editor-fold desc="CODE_BLOCK: Create Widget">
         bar = qt.QtWidgets.QMenuBar()
-
-        bar_main = bar.addMenu("功能選單")
-        bar_undo = qt.QtWidgets.QAction("/ 復原")
-        bar_redo = qt.QtWidgets.QAction("/ 重做")
-        m_help = bar.addMenu("👈 懶人用")
-        m_help.setEnabled(False)
-
-        bar_main_reset = qt.QtWidgets.QAction("重新載入")
-
+        action_reset = qt.QtWidgets.QAction("重新載入")
         tab = qt.QtTabCSWidget()
         tab_opt = qt.QtTabItemCSWidget()
-
-        frm_opt_mats = qt.QtFrameLayoutCSWidget(text="材質球　⚽")
-
-        # ui_opt_mats = module.OptimizeMaterialsCSWidget()
+        ui_set_pdir = module.SetProjectDirectoryCSWidget()
         # </editor-fold>
 
         # <editor-fold desc="CODE_BLOCK: Assembly Widget">
-        tab_opt.layout.addWidget(frm_opt_mats)
-
-        tab.addTab(tab_opt, "最佳化")
-
-        # frm_opt_mats.frame_layout.addWidget(ui_opt_mats)
-
+        tab_opt.layout.addWidget(ui_set_pdir)
+        tab.addTab(tab_opt, "讀取")
         layout.addWidget(tab)
-
-        bar_main.addAction(bar_main_reset)
-        bar.addMenu(bar_main)
-        bar.addAction(bar_undo)
-        bar.addAction(bar_redo)
-        bar.addMenu(m_help)
-
+        bar.addAction(action_reset)
         self.setLayout(layout)
-
         self.layout().setMenuBar(bar)
         # </editor-fold>
 
-        # <editor-fold desc="CODE_BLOCK: Public Variable">
+        # <editor-fold desc="CODE_BLOCK: Expose Variable">
         self.layout = layout
-
         self.bar = bar
-        self.bar_main = bar_main
-        self.bar_undo = bar_undo
-        self.bar_redo = bar_redo
-        self.bar_main_reset = bar_main_reset
-
         self.tab = tab
         self.tab_tool = tab_opt
-
-        self.frm_optimize = frm_opt_mats
-
-        # self.ui_optimize = ui_opt_mats
-
+        self.action_reset = action_reset
         # </editor-fold>
--- bim/ui/ui_tweak.py
+++ bim/ui/ui_tweak.py
@@ -6,8 +6,5 @@ class Tweak(ui_setup.Setup):
     def __init__(self):
         super(Tweak, self).__init__()
 
-        self.bar_main_reset.triggered.connect(gui.show)
-        self.bar_main_reset.setShortcut('Alt+`')
-
-        self.bar_undo.triggered.connect(gui.undo)
-        self.bar_redo.triggered.connect(gui.redo)
+        self.action_reset.triggered.connect(gui.show)
+        self.action_reset.setShortcut('Alt+`')
